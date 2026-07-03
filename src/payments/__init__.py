cf3985ae74dc65b65741c372849ea1651d66cfd2
Berk <berk.com14@gmail.com>
2026-07-03 13:43:43 +0300

    __init__.py

--- src/payments/__init__.py
+++ src/payments/__init__.py
@@ -1,7 +1,9 @@
 from src.payments.payment_service import PaymentService
 from src.payments.yoomoney_client import yoomoney_client
+from src.payments.generation_package import GenerationPackage
 
 __all__ = [
     "PaymentService",
     "yoomoney_client",
+    "GenerationPackage",
 ]
