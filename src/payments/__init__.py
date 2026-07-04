00e633acf1a8583e2263998b5b61e14651334b37
Berk <berk.com14@gmail.com>
2026-07-04 18:09:13 +0300

    __init__.py

--- src/payments/__init__.py
+++ src/payments/__init__.py
@@ -5,15 +5,15 @@ from src.payments.packages import (
 )
 
 from src.payments.payment_service import PaymentService
-from src.payments.yoomoney_client import yoomoney_client, YooMoneyClient
-from src.payments.webhook import register_webhook_routes
 
-__all__ = [
+from src.payments.webhook import (
+    register_webhook_routes,
+)
+
+__all__ = (
     "GenerationPackage",
     "GENERATION_PACKAGES",
     "get_package",
     "PaymentService",
-    "yoomoney_client",
-    "YooMoneyClient",
     "register_webhook_routes",
-]
+)
