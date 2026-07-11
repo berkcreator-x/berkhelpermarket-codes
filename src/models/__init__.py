edd7870f2578f88abc245cbffa272b867d92ba04
Berk <berk.com14@gmail.com>
2026-07-11 23:37:34 +0300

    __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,14 +1,12 @@
 from src.models.user import User
 from src.models.payment import Payment
 from src.models.generation_log import GenerationLog
-from src.models.generation_history import GenerationHistory
 from src.models.enums import PaymentStatus, GenerationType, PlanType
 
 __all__ = [
     "User",
     "Payment",
     "GenerationLog",
-    "GenerationHistory",
     "PaymentStatus",
     "GenerationType",
     "PlanType",
